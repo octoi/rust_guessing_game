636bfcadd6461649b6ca27b8c9ef55c00d77e1a3
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-08 07:24:06 +0530

    random number & logic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("Please input your guess.");
 
@@ -11,6 +15,14 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to readline");
+    
+    let guess: i32 = guess.trim().parse().expect("Please type a number");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
